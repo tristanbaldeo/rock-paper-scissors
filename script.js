b4a2869347d620c9900563ac306e20ffc9851c59
tristanbaldeo <113404111+tristanbaldeo@users.noreply.github.com>
2024-05-25 05:25:03 -0400

    added getHumanChoice function which enables player input

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-let choice = Math.floor(Math.random() * 3);
-
-function getComputerChoice () {
-    if (choice == 0) {
-        return "Rock";
+function getComputerChoice() {
+    let cpuChoice = Math.floor(Math.random() * 3);
+    if (cpuChoice == 0) {
+        return "rock";
     }
-    else if (choice == 1) {
-        return "Paper";
+    else if (cpuChoice == 1) {
+        return "paper";
     }
     else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
+function getHumanChoice() {
+    let huChoice = prompt("Rock, paper, or scissors?")
+    if (huChoice.toLowerCase() == "rock") {
+        return "You picked rock!"
+    } else if (huChoice.toLowerCase() == "paper") {
+        return "You picked paper!"
+    } else if (huChoice.toLowerCase() == "scissors") {
+        return "You picked scissors!"
+    } else {
+        return "Invalid option. Please try again!"
+    }
+}
+
+console.log(getHumanChoice());
 console.log(getComputerChoice());

501a2009d28f078888ff46b4f0dcd55d4251dbf7
tristanbaldeo <113404111+tristanbaldeo@users.noreply.github.com>
2024-05-25 03:36:09 -0400

    created getComputerChoice function which selects rock, paper, or scissors at random for the cpu

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello, World!')
+let choice = Math.floor(Math.random() * 3);
+
+function getComputerChoice () {
+    if (choice == 0) {
+        return "Rock";
+    }
+    else if (choice == 1) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
